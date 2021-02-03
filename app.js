6270b94db55c19697864ebf0e469f605f8f70c7d
Sayantan Samanta <sssamanta789@gmail.com>
2021-02-04 00:53:56 +0530

    Added more formating in boilerplate code

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = 3000
+const PORT = 5000
 
 app.set('view engine','pug');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('styles'));
+app.use(express.static('static'));
 
 
 app.get('/',(req,res) => {
