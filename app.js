337b1f29e618601264c97b5ef6419300c3a980a7
Sayantan Samanta <sssamanta789@gmail.com>
2021-07-26 00:05:55 +0530

    Added some basic blocking code

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const middlewares = [
 app.use(middlewares);
 
 app.get('/',(req,res) => {
+    (function (duration){
+        const start = Date.now();
+        while(Date.now() - start < duration){}
+    })(5000)
     res.end('<h1> Hello </h1>');
 })
 
